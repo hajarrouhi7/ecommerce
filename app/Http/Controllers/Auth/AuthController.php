675ca14d607db567bc4be4f1db138a535574261e
hajarrouhi7 <hajarrouhi7@gmail.com>
2023-05-15 23:28:35 +0100

    modify

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -1,10 +1,4 @@
-echo "# ecommerce" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/hajarrouhi7/ecommerce.git
-git push -u origin main<?php
+<?php
 
 namespace App\Http\Controllers\Auth;
 
